3e999beb1b886c3d89beef34bb29155f1d63128c
shikari\rehan <rehanyousaf147@gmail.com>
2018-11-20 01:32:48 +0500

    Members List & Details, Route Resolvers, Auto Auth Header, Photo Gallery

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map} from 'rxjs/operators';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5000/api/auth/';
+  baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
@@ -22,14 +23,14 @@ export class AuthService {
           if (user) {
             localStorage.setItem('token', user.token);
             this.decodedToken = this.jwtHelper.decodeToken(user.token);
-            console.log(this.decodedToken);
+            // console.log(this.decodedToken);x
           }
         })
       );
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl+'register', model);
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
   loggedIn() {
